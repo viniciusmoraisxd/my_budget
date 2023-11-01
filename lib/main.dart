e5420e54866dd0ae3a215e9e8ed3c45116dc6b79
Vinícius Ramos de Morais <viniciusmoraisxd@gmail.com>
2023-10-31 21:32:16 -0300

    Initial configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'view/pages/sign_in/sign_in_page.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -11,11 +13,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'My Budget App',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: Container(),
+      home: const SignInPage(),
     );
   }
 }
